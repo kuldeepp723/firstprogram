43e522346bd15b8cfefe369c617318fb450e9987
kuldeepp723 <kuldeepp723@gmail.com>
2017-10-02 20:08:59 +0530

    SECOND

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,14 @@
 
 public class Hello {
 	
+	public void display()
+	{
+		System.out.println("GIT GIT");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("HELLO GIT");
+		
 	}
 
 }
